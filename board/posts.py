c205edfec6276eb6acc0890ffb7d69a7eb3c892e
igopratama112 <pratamamuhammadigo@gmail.com>
2024-09-16 21:46:05 +0700

    berhasil

--- board/posts.py
+++ board/posts.py
@@ -1,9 +1,30 @@
-from flask import Blueprint, render_template
+from flask import (
+    Blueprint,
+    redirect,
+    render_template,
+    request,
+    url_for,
+)
+
+from board.database import get_db
 
 bp = Blueprint("posts", __name__)
 
 @bp.route("/create", methods=("GET", "POST"))
 def create():
+    if request.method == "POST":
+        author = request.form["author"] or "Anonymous"
+        message = request.form["message"]
+
+        if message:
+            db = get_db()
+            db.execute(
+                "INSERT INTO post (author, message) VALUES (?, ?)",
+                (author, message),
+            )
+            db.commit()
+            return redirect(url_for("posts.posts"))
+
     return render_template("posts/create.html")
 
 @bp.route("/posts")
